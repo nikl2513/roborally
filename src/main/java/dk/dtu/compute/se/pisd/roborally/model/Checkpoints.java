6329c6a817f9acb6f9580e10f69cb39127d1dae1
Nikolai Travis <s224567@student.dtu.dk>
2023-03-15 15:17:32 +0100

    Checkpoint

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Checkpoints.java
@@ -0,0 +1,21 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+public class Checkpoints {
+
+    private Space space;
+
+    public Space getSpace() {
+        return space;
+    }
+
+    public void setSpace(Space space) {
+        this.space = space;
+    }
+
+    public void Checkpoint(){
+        space = null;
+        boolean complete = false;
+    }
+    public void setCheckpoint(){
+    }
+}
--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -22,7 +22,6 @@
 package dk.dtu.compute.se.pisd.roborally.view;
 
 import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
-import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 import javafx.scene.canvas.Canvas;
